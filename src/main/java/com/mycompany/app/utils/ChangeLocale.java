f31b2845e592cebfc4698ad4f98e221fcb955de5
lalime <lionnelnana@ymail.com>
2013-05-02 11:03:40 +0200

    implement the ChangeLocale.java class

--- src/main/java/com/mycompany/app/utils/ChangeLocale.java
+++ src/main/java/com/mycompany/app/utils/ChangeLocale.java
@@ -4,10 +4,34 @@
  */
 package com.mycompany.app.utils;
 
+import java.io.Serializable;
+import javax.annotation.ManagedBean;
+import javax.enterprise.context.SessionScoped;
+
 /**
  *
  * @author Lalime
  */
-public class ChangeLocale {
-    
-}
+
+@ManagedBean 
+@SessionScoped 
+public class ChangeLocale implements Serializable{ 
+
+  private String locale="fr"; 
+
+
+  public ChangeLocale() { 
+
+  } 
+
+ 
+
+  public String getLocale() { 
+
+    return locale; 
+
+  } 
+
+   
+
+} 
