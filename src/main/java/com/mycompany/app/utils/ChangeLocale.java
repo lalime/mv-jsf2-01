43a7f12dabcc9ff2cf88f6965ad69a4873453751
lalime <lionnelnana@ymail.com>
2013-05-02 10:51:36 +0200

    New Files for project configuration and views

---
+++ src/main/java/com/mycompany/app/utils/ChangeLocale.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.app.utils;
+
+/**
+ *
+ * @author Lalime
+ */
+public class ChangeLocale {
+    
+}
